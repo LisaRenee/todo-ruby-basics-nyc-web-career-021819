06aa250176423ddec0820ca822af32ec1ff2d6bb
Lisa Scott <lrscot@aol.com>
2019-02-02 04:31:40 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ 30 / 5
 end
 
-def assign_variable(value)
-  
+def assign_variable(value = "Bob")
+  assign_variable(value)
 end
 
-def argue
-  
+def argue(Lisa)
+  argue
 end
 
-def greeting
-
+def greeting("Hello", name)
+  puts greeting
 end
 
-def return_a_value
-
+def return_a_value(phrase = "Nice")
+  return_a_value
 end
 
 def last_evaluated_value
